407bdaa48d73124f6c25456a665082b83af6edbb
DodyDharma440 <dharma.dody@yahoo.com>
2023-02-26 20:16:15 +0800

    Initial Commit

---
+++ HomeRent/HomeRentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeRentApp.swift
+//  HomeRent
+//
+//  Created by Dodi Aditya on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeRentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
